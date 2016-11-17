115ce9df0400d30d5bb0ef7c2d7739f9952fd78c
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-17 00:07:05 +0000

    Finished the form validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,3 @@
-// var data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
 var data = [];
 
 function renderTweets(tweets) {
@@ -52,6 +5,12 @@ function renderTweets(tweets) {
   tweets.forEach( (tweet) => {
     $('#old-tweets').append(createTweetElement(tweet))
   })
+}
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 }
 
 function createTweetElement(tweet) {
@@ -68,7 +27,7 @@ function createTweetElement(tweet) {
       <h3 class="username">${userName}</h3>
       <h6 class="user-handle">${userHandle}</h6>
     </header>
-    <p id="tweets-container">${oldTweet}</p>
+    <p id="tweets-container">${escape(oldTweet)}</p>
     <footer>
       ${datePosted}
       <div class="icons">
@@ -87,17 +46,24 @@ $(document).ready(function() {
 
   $('form').on('submit', function(event){
     event.preventDefault();
-    $.ajax("/tweets", {
-      method: "POST",
-      data:  $(this).serialize()
-    }).done(function(){});
-  })
+    var tweet = $(this).serialize();
+    if (tweet.slice(5) === '') {
+      alert('Please enter a message in the tweet box!');
+    } else if (tweet.slice(5).length >= 141) {
+      alert('Your tweet is too long! The max character length is 140. Please remove some characters!');
+    } else {
+      $.ajax("/tweets", {
+        method: "POST",
+        data: tweet
+      })
+    }
+  });
 
 const loadTweets = function (){
   $.ajax('http://localhost:8080/tweets', {
     method: "GET"
   }).done(function(res){
-    renderTweets(res)
+    renderTweets(res);
   });
 }
 loadTweets();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,20 +4,14 @@ const newTweet = function() {
 
   tweetText.on('keyup', function(event){
     let charLength = $(this).val().length;
-    currentCharLength.text(140 - charLength);
+    currentCharLength.html(140 - charLength);
+
     if (currentCharLength.html() < 0){
       currentCharLength.css( { 'color': 'red'} )
-    };
-  });
-
-  tweetText.on('keydown', function(event) {
-    if (currentCharLength.text() >= 0){
+    } else {
       currentCharLength.css( { 'color': 'black'} )
-    } else if ((event.key === "Backspace")) {
-      currentCharLength.text()++;
-    };
+    }
   });
-
 };
 
 $(newTweet);
